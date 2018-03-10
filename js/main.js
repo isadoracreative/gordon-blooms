45e3452aa8f07cf4e9d06d8dd27a44071ee4bbc6
Isadora Williams <isadora.williams@gmail.com>
2018-03-10 15:40:18 -0500

    added credits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/* Script for Gordon Florist Website Proposal */
+/* Website visual design and front-end development by Isadora Williams, copyright 2018 */
+/* isadoracreative.com */
+
 function initMap() {
         let gordonFlorist = {lat: 42.66635, lng: -70.835535};
         let map = new google.maps.Map(document.getElementById('map'), {
